3757be1dd3a818d564159e2034909ef607411ebf
matome esrom mogashoa <you@example.com>
2024-09-26 01:53:28 +0200

    update

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ $db_username = "root"; // Your database username
 $db_password = ""; // Your database password
 $dbname = "report-smart";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($server_name, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
